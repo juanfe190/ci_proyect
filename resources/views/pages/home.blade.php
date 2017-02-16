cdde96136a594d36387b9f16538c87dc8c5bc37e
kenneth <k.chevezramirez@gmail.com>
2017-02-15 23:54:44 -0600

    Se avanza en el diseño de los cards. Se diseña el título, tipo, categoría, etapas y dificultad

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -10,24 +10,92 @@
 	  Your browser does not support the video tag.
 	</video>-->
 	<div class="row">
-		<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+		<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 card-space">
 			<a href="" class="card">
 				<div class="card-colored-side bg-red"></div>
+				<div class="card-divider"></div>
+				<div class="card-title">
+					<h3 class="card-title-type">Curso</h3>
+					<h1 class="card-title-name font-red">PHP Básico</h1>
+				</div>
+				<div class="card-stats card-category">
+					<h4>Categoría</h4>
+					<p>PHP</p>
+				</div>
+				<div class="card-stats card-stage">
+					<h4>Etapas</h4>
+					<p>5</p>
+				</div>
+				<div class="card-stats card-difficulty">
+					<h4>Dificultad</h4>
+					<p>Principiante</p>
+				</div>
 			</a>
 		</div>
-		<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+		<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 card-space">
 			<a href="" class="card">
 				<div class="card-colored-side bg-green-meadow"></div>
+				<div class="card-divider"></div>
+				<div class="card-title">
+					<h3 class="card-title-type">Curso</h3>
+					<h1 class="card-title-name font-green-meadow">Cómo crear tu propia empresa</h1>
+				</div>
+				<div class="card-stats card-category">
+					<h4>Categoría</h4>
+					<p>Negocios</p>
+				</div>
+				<div class="card-stats card-stage">
+					<h4>Etapas</h4>
+					<p>3</p>
+				</div>
+				<div class="card-stats card-difficulty">
+					<h4>Dificultad</h4>
+					<p>Avanzado</p>
+				</div>
 			</a>
 		</div>
-		<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+		<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 card-space">
 			<a href="" class="card">
 				<div class="card-colored-side bg-yellow-saffron"></div>
+				<div class="card-divider"></div>
+				<div class="card-title">
+					<h3 class="card-title-type">Curso</h3>
+					<h1 class="card-title-name font-yellow-saffron">Collecciones Java</h1>
+				</div>
+				<div class="card-stats card-category">
+					<h4>Categoría</h4>
+					<p>Java</p>
+				</div>
+				<div class="card-stats card-stage">
+					<h4>Etapas</h4>
+					<p>17</p>
+				</div>
+				<div class="card-stats card-difficulty">
+					<h4>Dificultad</h4>
+					<p>Intermedio</p>
+				</div>
 			</a>
 		</div>
-		<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+		<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 card-space">
 			<a href="" class="card">
 				<div class="card-colored-side bg-purple"></div>
+				<div class="card-divider"></div>
+				<div class="card-title">
+					<h3 class="card-title-type">Curso</h3>
+					<h1 class="card-title-name font-purple">Publicidad: Básico</h1>
+				</div>
+				<div class="card-stats card-category">
+					<h4>Categoría</h4>
+					<p>Marketing</p>
+				</div>
+				<div class="card-stats card-stage">
+					<h4>Etapas</h4>
+					<p>3</p>
+				</div>
+				<div class="card-stats card-difficulty">
+					<h4>Dificultad</h4>
+					<p>Principiante</p>
+				</div>
 			</a>
 		</div>
 	</div>
