4206a31ee1807281758c49298d366658032f761e
kenneth <k.chevezramirez@gmail.com>
2017-02-18 20:18:08 -0600

    Se mejora cards, y se termina el diseño de los bookmark.

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -10,93 +10,255 @@
 	  Your browser does not support the video tag.
 	</video>-->
 	<div class="row">
-		<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 card-space">
-			<a href="" class="card">
-				<div class="card-colored-side bg-red"></div>
-				<div class="card-divider"></div>
-				<div class="card-title">
-					<h3 class="card-title-type">Curso</h3>
-					<h1 class="card-title-name font-red">PHP Básico</h1>
-				</div>
-				<div class="card-stats card-category">
-					<h4>Categoría</h4>
-					<p>PHP</p>
-				</div>
-				<div class="card-stats card-stage">
-					<h4>Etapas</h4>
-					<p>5</p>
-				</div>
-				<div class="card-stats card-difficulty">
-					<h4>Dificultad</h4>
-					<p>Principiante</p>
-				</div>
-			</a>
+		<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+			<div class="card-space category-1 bookmarked">
+				<a href="" class="card">
+					<div class="card-colored-side"></div>
+					<div class="card-divider"></div>
+					<div class="card-title">
+						<h3 class="card-title-type">Curso</h3>
+						<h1 class="card-title-name">PHP Básico</h1>
+					</div>
+					<div class="card-stats card-stage">
+						<h4>Etapas</h4>
+						<p>5</p>
+					</div>
+					<div class="card-stats card-category">
+						<h4>Categoría</h4>
+						<p>PHP</p>
+					</div>
+					<div class="card-stats card-difficulty">
+						<h4>Dificultad</h4>
+						<p>Principiante</p>
+					</div>
+				</a>
+				<a href="" class="card-bookmark"></a>
+			</div>
 		</div>
-		<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 card-space">
-			<a href="" class="card">
-				<div class="card-colored-side bg-green-meadow"></div>
-				<div class="card-divider"></div>
-				<div class="card-title">
-					<h3 class="card-title-type">Curso</h3>
-					<h1 class="card-title-name font-green-meadow">Cómo crear tu propia empresa</h1>
-				</div>
-				<div class="card-stats card-category">
-					<h4>Categoría</h4>
-					<p>Negocios</p>
-				</div>
-				<div class="card-stats card-stage">
-					<h4>Etapas</h4>
-					<p>3</p>
-				</div>
-				<div class="card-stats card-difficulty">
-					<h4>Dificultad</h4>
-					<p>Avanzado</p>
-				</div>
-			</a>
+		<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+			<div class="card-space category-2">
+				<a href="" class="card">
+					<div class="card-colored-side"></div>
+					<div class="card-divider"></div>
+					<div class="card-title">
+						<h3 class="card-title-type">Curso</h3>
+						<h1 class="card-title-name">Cómo crear tu propia empresa</h1>
+					</div>
+					<div class="card-stats card-stage">
+						<h4>Etapas</h4>
+						<p>3</p>
+					</div>
+					<div class="card-stats card-category">
+						<h4>Categoría</h4>
+						<p>Negocios</p>
+					</div>
+					<div class="card-stats card-difficulty">
+						<h4>Dificultad</h4>
+						<p>Avanzado</p>
+					</div>
+				</a>
+				<a href="" class="card-bookmark"></a>
+			</div>
 		</div>
-		<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 card-space">
-			<a href="" class="card">
-				<div class="card-colored-side bg-yellow-saffron"></div>
-				<div class="card-divider"></div>
-				<div class="card-title">
-					<h3 class="card-title-type">Curso</h3>
-					<h1 class="card-title-name font-yellow-saffron">Collecciones Java</h1>
-				</div>
-				<div class="card-stats card-category">
-					<h4>Categoría</h4>
-					<p>Java</p>
-				</div>
-				<div class="card-stats card-stage">
-					<h4>Etapas</h4>
-					<p>17</p>
-				</div>
-				<div class="card-stats card-difficulty">
-					<h4>Dificultad</h4>
-					<p>Intermedio</p>
-				</div>
-			</a>
+		<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+			<div class="card-space category-3">
+				<a href="" class="card">
+					<div class="card-colored-side"></div>
+					<div class="card-divider"></div>
+					<div class="card-title">
+						<h3 class="card-title-type">Curso</h3>
+						<h1 class="card-title-name">Collecciones Java</h1>
+					</div>
+					<div class="card-stats card-stage">
+						<h4>Etapas</h4>
+						<p>17</p>
+					</div>
+					<div class="card-stats card-category">
+						<h4>Categoría</h4>
+						<p>Java</p>
+					</div>
+					<div class="card-stats card-difficulty">
+						<h4>Dificultad</h4>
+						<p>Intermedio</p>
+					</div>
+				</a>
+				<a href="" class="card-bookmark"></a>
+			</div>
 		</div>
-		<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 card-space">
-			<a href="" class="card">
-				<div class="card-colored-side bg-purple"></div>
-				<div class="card-divider"></div>
-				<div class="card-title">
-					<h3 class="card-title-type">Curso</h3>
-					<h1 class="card-title-name font-purple">Publicidad: Básico</h1>
-				</div>
-				<div class="card-stats card-category">
-					<h4>Categoría</h4>
-					<p>Marketing</p>
-				</div>
-				<div class="card-stats card-stage">
-					<h4>Etapas</h4>
-					<p>3</p>
-				</div>
-				<div class="card-stats card-difficulty">
-					<h4>Dificultad</h4>
-					<p>Principiante</p>
-				</div>
-			</a>
+		<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+			<div class="card-space category-4">
+				<a href="" class="card">
+					<div class="card-colored-side"></div>
+					<div class="card-divider"></div>
+					<div class="card-title">
+						<h3 class="card-title-type">Curso</h3>
+						<h1 class="card-title-name">Publicidad: Básico</h1>
+					</div>
+					<div class="card-stats card-stage">
+						<h4>Etapas</h4>
+						<p>3</p>
+					</div>
+					<div class="card-stats card-category">
+						<h4>Categoría</h4>
+						<p>Marketing</p>
+					</div>
+					<div class="card-stats card-difficulty">
+						<h4>Dificultad</h4>
+						<p>Principiante</p>
+					</div>
+				</a>
+				<a href="" class="card-bookmark"></a>
+			</div>
+		</div>
+		<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+			<div class="card-space category-5">
+				<a href="" class="card">
+					<div class="card-colored-side"></div>
+					<div class="card-divider"></div>
+					<div class="card-title">
+						<h3 class="card-title-type">Curso</h3>
+						<h1 class="card-title-name">Publicidad: Básico</h1>
+					</div>
+					<div class="card-stats card-stage">
+						<h4>Etapas</h4>
+						<p>3</p>
+					</div>
+					<div class="card-stats card-category">
+						<h4>Categoría</h4>
+						<p>Marketing</p>
+					</div>
+					<div class="card-stats card-difficulty">
+						<h4>Dificultad</h4>
+						<p>Principiante</p>
+					</div>
+				</a>
+				<a href="" class="card-bookmark"></a>
+			</div>
+		</div>
+		<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+			<div class="card-space category-6">
+				<a href="" class="card">
+					<div class="card-colored-side"></div>
+					<div class="card-divider"></div>
+					<div class="card-title">
+						<h3 class="card-title-type">Curso</h3>
+						<h1 class="card-title-name">Publicidad: Básico</h1>
+					</div>
+					<div class="card-stats card-stage">
+						<h4>Etapas</h4>
+						<p>3</p>
+					</div>
+					<div class="card-stats card-category">
+						<h4>Categoría</h4>
+						<p>Marketing</p>
+					</div>
+					<div class="card-stats card-difficulty">
+						<h4>Dificultad</h4>
+						<p>Principiante</p>
+					</div>
+				</a>
+				<a href="" class="card-bookmark"></a>
+			</div>
+		</div>
+		<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+			<div class="card-space category-7">
+				<a href="" class="card">
+					<div class="card-colored-side"></div>
+					<div class="card-divider"></div>
+					<div class="card-title">
+						<h3 class="card-title-type">Curso</h3>
+						<h1 class="card-title-name">Publicidad: Básico</h1>
+					</div>
+					<div class="card-stats card-stage">
+						<h4>Etapas</h4>
+						<p>3</p>
+					</div>
+					<div class="card-stats card-category">
+						<h4>Categoría</h4>
+						<p>Marketing</p>
+					</div>
+					<div class="card-stats card-difficulty">
+						<h4>Dificultad</h4>
+						<p>Principiante</p>
+					</div>
+				</a>
+				<a href="" class="card-bookmark"></a>
+			</div>
+		</div>
+		<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+			<div class="card-space category-8">
+				<a href="" class="card">
+					<div class="card-colored-side"></div>
+					<div class="card-divider"></div>
+					<div class="card-title">
+						<h3 class="card-title-type">Curso</h3>
+						<h1 class="card-title-name">Publicidad: Básico</h1>
+					</div>
+					<div class="card-stats card-stage">
+						<h4>Etapas</h4>
+						<p>3</p>
+					</div>
+					<div class="card-stats card-category">
+						<h4>Categoría</h4>
+						<p>Marketing</p>
+					</div>
+					<div class="card-stats card-difficulty">
+						<h4>Dificultad</h4>
+						<p>Principiante</p>
+					</div>
+				</a>
+				<a href="" class="card-bookmark"></a>
+			</div>
+		</div>
+		<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+			<div class="card-space category-9">
+				<a href="" class="card">
+					<div class="card-colored-side"></div>
+					<div class="card-divider"></div>
+					<div class="card-title">
+						<h3 class="card-title-type">Curso</h3>
+						<h1 class="card-title-name">Publicidad: Básico</h1>
+					</div>
+					<div class="card-stats card-stage">
+						<h4>Etapas</h4>
+						<p>3</p>
+					</div>
+					<div class="card-stats card-category">
+						<h4>Categoría</h4>
+						<p>Marketing</p>
+					</div>
+					<div class="card-stats card-difficulty">
+						<h4>Dificultad</h4>
+						<p>Principiante</p>
+					</div>
+				</a>
+				<a href="" class="card-bookmark"></a>
+			</div>
+		</div>
+		<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+			<div class="card-space category-10">
+				<a href="" class="card">
+					<div class="card-colored-side"></div>
+					<div class="card-divider"></div>
+					<div class="card-title">
+						<h3 class="card-title-type">Curso</h3>
+						<h1 class="card-title-name">Publicidad: Básico</h1>
+					</div>
+					<div class="card-stats card-stage">
+						<h4>Etapas</h4>
+						<p>3</p>
+					</div>
+					<div class="card-stats card-category">
+						<h4>Categoría</h4>
+						<p>Marketing</p>
+					</div>
+					<div class="card-stats card-difficulty">
+						<h4>Dificultad</h4>
+						<p>Principiante</p>
+					</div>
+				</a>
+				<a href="" class="card-bookmark"></a>
+			</div>
 		</div>
 	</div>
 	
